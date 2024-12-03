89a954dcd092db16ba52a7f896ffee871cca1e3a
Kyuss Poelmans <kyuss.poelmans@atheneumsinttruiden.be>
2024-12-03 11:14:10 +0100

    Update wijzig.php

--- includes/wijzig.php
+++ includes/wijzig.php
@@ -1,6 +1,8 @@
 <div class="jumbotron">
 	<h1 class="display-4">Update hier je taak!</h1>
 </div>
+<!-- Nakijken of form al is ingevuld of niet? 
+Niet: info opvragen aan db adhv $_GET["id"]-->
 <form method="post" action="begin.php?page=wijzig">
 	<div class="row">
 		<div class="col tegel">
@@ -8,7 +10,7 @@
 				<div class="form-group row">
 					<div class="col">
 						<label for="UmakerN">Wijzig Naam Maker:</label><br>
-						<p><input type="text" id="UmakerN" class="form-control"name="UmakerN"></p>
+						<p><input type="text" id="UmakerN" class="form-control"name="UmakerN" value="<?php $row["NaamMaker"]; ?>"></p>
 					</div>
 				</div>
 				
